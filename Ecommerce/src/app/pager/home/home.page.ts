4dbf49910cce394344831a08a598ad21ca7b1bdd
judynatalialopez <137812850+judynatalialopez@users.noreply.github.com>
2025-08-28 11:15:02 -0500

    ajuste

--- Ecommerce/src/app/pager/home/home.page.ts
+++ Ecommerce/src/app/pager/home/home.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { ListaProductosComponent } from 'src/app/components/lista-productos/lista-productos.component';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ListaProductosComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
--- Ecommerce/src/app/pager/list/list.page.ts
+++ Ecommerce/src/app/pager/list/list.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ListaProductosComponent } from 'src/app/components/lista-productos/lista-productos.component';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ListaProductosComponent]
 })
 export class ListPage implements OnInit {
 
