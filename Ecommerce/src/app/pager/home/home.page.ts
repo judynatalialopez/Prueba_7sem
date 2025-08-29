5f311a9e8ace670319fb9720be8ce3e9ce85ab72
juan62 <juan@bluecell.co>
2025-08-28 19:40:25 -0500

    ajuste diseño

--- Ecommerce/src/app/pager/home/home.page.ts
+++ Ecommerce/src/app/pager/home/home.page.ts
@@ -11,7 +11,27 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
+
 export class HomePage implements OnInit {
+  
+  titulo: string ="Bookshop";
+  logoImage: string = "assets/img/logo.jpeg";
+
+
+  usuario: string = '';
+  direccion: string = '';
+  pais: string = '';
+  idioma: string = '';
+  moneda: string = '';
+
+  imprimir() {
+    console.log('Datos ingresados:');
+    console.log('Usuario:', this.usuario);
+    console.log('Dirección de entrega:', this.direccion);
+    console.log('País y ciudad del usuario:', this.pais);
+    console.log('Idioma:', this.idioma);
+    console.log('Moneda:', this.moneda);
+  }
 
   constructor() { }
 
